f9dc54cf115d8bf75bc91d1789e481cab763a8d2
jitverma <30344435+jitverma@users.noreply.github.com>
2023-08-25 17:09:47 +0530

    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast menu')
 streamlit.text('Bread Toast')
 streamlit.text('Omega 3 and blueberry oatmeal')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
